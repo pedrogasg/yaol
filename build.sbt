72e7e99d504ff76f0866cb00111f88c7ed25fa00
Pedro <pedrogas_g@hotmail.com>
2016-10-25 01:36:17 +0200

    scala test reports

--- build.sbt
+++ build.sbt
@@ -7,8 +7,14 @@ lazy val akkaVersion = "2.4.11"
 
 fork in Test := true
 
+testOptions in Test ++= Seq(
+  Tests.Argument(TestFrameworks.ScalaTest, "-u", "target/test-reports"),
+  Tests.Argument(TestFrameworks.ScalaTest, "-h", "target/test-reports")
+)
+
 libraryDependencies ++= Seq(
   "com.typesafe.akka" %% "akka-actor" % akkaVersion,
   "com.typesafe.akka" %% "akka-testkit" % akkaVersion % "test",
+  "org.pegdown" % "pegdown" % "1.6.0" % "test",
   "org.scalatest" %% "scalatest" % "3.0.0" % "test"
 )
--- src/test/scala/yaol/StochasticHillClimbingSpec.scala
+++ src/test/scala/yaol/StochasticHillClimbingSpec.scala
@@ -8,8 +8,12 @@ class StochasticHillClimbingSpec extends FlatSpec with Matchers {
   val maxIteration = 100
   val positivesBits = 64
 
-  "A Stochastic Hill Climbing search " should "return a nuber close to the positives bits" in {
+  "A Stochastic Hill Climbing search " should "return a number close to the positives bits" in {
     val o = StochasticHillClimbing.search(positivesBits, maxIteration)
     o.cost should be > (positivesBits - 10.0)
+  }
+  it should "return the positivesBits with a large maxIteration" in {
+    val o = StochasticHillClimbing.search(positivesBits, maxIteration * 10)
+    o.cost should be > (positivesBits - 2.0)
   }
 }
